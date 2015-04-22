0de499c702be5574e01dc8534874ad1f7a9c793a
jackchen <chqblhwfe@gmail.com>
2015-04-22 17:32:02 +0800

    perfect js doc
    perfect js doc,
    
    about “function moveElement”

--- scripts/global.js
+++ scripts/global.js
@@ -50,3 +50,34 @@ function highlightPage() {
 }
 addLoadEvent(highlightPage);
 
+function moveElement(elementID, final_x, final_y, interval){
+	if(!document.getElementById) return false;
+	if(!document.getElementById(elementID)) return false;
+	var elem = document.getElementById(elementID);
+	if (elem.movement){
+		
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
